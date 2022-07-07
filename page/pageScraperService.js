936fc8a8931f5076b5cb636487f3edf80859087f
Mateus <mateusneves2009@live.com>
2022-07-07 12:59:34 -0300

    Pega o link da página inicial no pageScraperService

--- page/pageScraperService.js
+++ page/pageScraperService.js
@@ -2,9 +2,18 @@ const scraperObject = {
     url: 'http://books.toscrape.com/',
     async scraper(browser) {
         let page = await browser.newPage();
-        console.log(`Navigating to ${this.url}...`);
-        await page.goto(this.url);
-    }
+            console.log(`Navigating to ${this.url}...`);
+            await page.goto(this.url);
+            await page.waitForSelector('.page_inner');
+            // Get the link to all the required books
+            let urls = await page.$$eval('section ol > li', links => {
+            //  links = links.filter(link => link.querySelector('.instock.availability > p').textContent !== "In stock"); 
+                links = links.map(el => el.querySelector('h3 > a').href);
+                  links = links.map(price => price.querySelector('a > img').alt);
+                return links;
+            });
+        console.log(urls);
+        }
 }
 
 module.exports = scraperObject;

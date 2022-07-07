458865e82035427887870aa5ef7c1215fde4e666
Mateus <mateusneves2009@live.com>
2022-07-07 16:03:08 -0300

    Agora o service consegue pegar informações de todos os livros da primeira página

--- page/pageScraperService.js
+++ page/pageScraperService.js
@@ -7,13 +7,29 @@ const scraperObject = {
             await page.waitForSelector('.page_inner');
             // Get the link to all the required books
             let urls = await page.$$eval('section ol > li', links => {
-            //  links = links.filter(link => link.querySelector('.instock.availability > p').textContent !== "In stock"); 
+            //  links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock"); 
                 links = links.map(el => el.querySelector('h3 > a').href);
-                  links = links.map(price => price.querySelector('a > img').alt);
                 return links;
             });
-        console.log(urls);
+        let pagePromise = (link) => new Promise(async(resolve, reject) => {
+            let dataObj = {};
+            let newPage = await browser.newPage();
+            await newPage.goto(link);
+            dataObj['bookTitle'] = await newPage.$eval('.product_main h1', text => text.textContent);
+            dataObj['bookPrice'] = await newPage.$eval('.price_color', text => text.textContent);
+            dataObj['imageUrl'] = await newPage.$eval('#product_gallery img', i => i.src);
+            dataObj['bookDescription'] = await newPage.$eval('#product_description', div => div.nextSibling.nextSibling.textContent);
+            dataObj['upc'] = await newPage.$eval('.table.table-striped > tbody > tr > td', table => table.textContent);
+            resolve(dataObj);
+        });
+
+        for(link in urls){
+            let currentPageData = await pagePromise(urls[link]);
+            // scrapedData.push(currentPageData);
+            console.log(currentPageData);
         }
+    
+    }
 }
 
 module.exports = scraperObject;

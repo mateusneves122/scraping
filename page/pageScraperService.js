c18d42d365d1e691be55b2d23acf4e0df2f005ee
Mateus <mateusneves2009@live.com>
2022-07-08 00:09:42 -0300

    Concluído a etapa de pegar as informações de todos os livros do site

--- page/pageScraperService.js
+++ page/pageScraperService.js
@@ -4,6 +4,8 @@ const scraperObject = {
         let page = await browser.newPage();
             console.log(`Navigating to ${this.url}...`);
             await page.goto(this.url);
+        let scrapedData = [];
+        async function scrapeCurrentPage() {
             await page.waitForSelector('.page_inner');
             // Get the link to all the required books
             let urls = await page.$$eval('section ol > li', links => {
@@ -21,15 +23,35 @@ const scraperObject = {
             dataObj['bookDescription'] = await newPage.$eval('#product_description', div => div.nextSibling.nextSibling.textContent);
             dataObj['upc'] = await newPage.$eval('.table.table-striped > tbody > tr > td', table => table.textContent);
             resolve(dataObj);
+            newPage.close();
         });
 
-        for(link in urls){
-            let currentPageData = await pagePromise(urls[link]);
-            // scrapedData.push(currentPageData);
-            console.log(currentPageData);
+        for(const link in urls){
+                let currentPageData = await pagePromise(urls[link]);
+                scrapedData.push(currentPageData);
+                //console.log(currentPageData);
+            }
+            
+        let nextButtonExist = false;
+        try {
+                const nextButton = page.$eval('.next a', text => text.textContent);
+                nextButtonExist = true;
+        } catch(err) {
+                nextButtonExist = false;
         }
-    
+        if(nextButtonExist) {
+            await page.click('.next a');
+            return scrapeCurrentPage();
+        }
+        await page.close();
+        return scrapedData;
+
+        }
+        let data = await scrapeCurrentPage();
+        console.log(data);
+        return data;        
     }
+
 }
 
 module.exports = scraperObject;
